f0228d4b31ddb8ced01a80ce09047ce65ce45336
Mike Chadwick <mike@mikedigitize.com>
2015-06-09 11:15:49 +0100

    mongo initialised

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
+var ObjectId = require('mongodb').ObjectID;
+var databaseUrl = "mongodb://localhost:27017/callcentre";
+var collections = ["staff", "managers"];
+var db = require("mongojs").connect( databaseUrl, collections );
+
+
 server.listen(1337);
 app.use(express.static(__dirname + "/public" ));
 
@@ -16,3 +22,44 @@ io.on("connection", function(socket) {
     });
 
 });
+
+(function dbSetUp() {
+
+	db.managers.find(function(err, docs) {    
+    	if(!docs.length) {
+			insertManagers(); 
+		}    
+    });
+
+    db.staff.find(function(err, docs) {    
+    	if(!docs.length) {
+			insertUsers(); 
+		}    
+    });
+
+})();
+
+function insertUsers() {
+	console.log("insert staff here");
+}
+
+function insertManagers() {
+	db.managers.insert([{
+		name : "Colin Steele",
+		location : "UK BLT"
+	}, {
+		name : "Ste Ashworth",
+		location : "UK BLT"
+	}, {
+		name : "Michael Chadwick",
+		location : "UK BLT"
+	}, {
+		name : "Ben Ely",
+		location : "UK BLT"
+	}, {
+		name : "Christian Tute",
+		location : "UK BLT"
+	}], function( err, record ) {    
+    	err ? console.log( err ) : console.log( 'Success: ', record );
+    });
+}
