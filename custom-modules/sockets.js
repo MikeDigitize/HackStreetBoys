ea07f4864075f427ce4e54b8ba0f9bae38d06e2a
trsben <me@benely.co.uk>
2015-06-09 13:28:49 +0100

    Add function to handle getting a staff member's stats

--- custom-modules/sockets.js
+++ custom-modules/sockets.js
@@ -1,12 +1,19 @@
+var Staff = require('./staff.js');
+
 function socketStart(io, database) {
 
 	io.on("connection", function(socket) {
 	    io.to(socket.id).emit("userid", socket.id);
 
+	    // send Nialls stats as an example
+	    var Niall = new Staff('Niall Hanlon', database);
+	    Niall.getCalls(new Date(), function(calls) {
+	    	io.to(socket.id).emit("numCalls", calls.length);
+	    });
+
 	    socket.on("disconnect", function(){
 	        console.log("user disconnected", socket.id);
 	    });
-
 
 	});
 
